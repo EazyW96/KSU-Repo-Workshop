9eccd8553d3155381ff99e8751df4f1eb6233381
EazyW96 <130127932+EazyW96@users.noreply.github.com>
2025-09-26 03:20:37 -0400

    Scripts folder made with HelloBadge.cs

---
+++ GitHub_workshop/Assets/Scripts/HelloBadge.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HelloBadge : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello GitHub Badge from Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
