6bfc58fbfa1b3d8b111fcb35f2ed3c251f209939
EazyW96 <130127932+EazyW96@users.noreply.github.com>
2025-09-30 15:20:30 -0400

    helloName script added
    test run of unity console

--- GameUp/Assets/Scripts/MyFirstScript.cs
+++ GameUp/Assets/Scripts/MyFirstScript.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+
+    public int helloName(string name)
+    {
+        Debug.Log("Hello " + name);
+        return 0;
+    }   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        helloName("World");
     }
 
     // Update is called once per frame
